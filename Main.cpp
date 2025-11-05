5758a7e945392d9512080e0db392f8429cd95def
Akul <121106780+Akul-R@users.noreply.github.com>
2025-11-05 14:58:55 +0000

    Cleaned up main .cpp file (again)
    Now its even more beautiful (beautifuller ??)

--- Main.cpp
+++ Main.cpp
@@ -5,15 +5,15 @@ int main() {
 	int out_epoch = 500; //how many epochs between prints
 
 	const int bits = 4; //specify how many bit input you are doing. bear in mind, larger bits take a lot longer to train properly
-
 	double training_rate = 0.01; //i hate this f*cking variable so much (i set it to 0 by accident and spent 3+ hours wondering why my network was getting dumber with each epoch)
+	
 	ANN NN1({4, 8, 16}, "relu", bits); //first arg is vector for layer and their sizes, 2nd is activation function and 3rd is number of bits
-	int epoch = 0;
 
 	NN1.training_start(max_epoch, out_epoch, training_rate);
 	NN1.save("4BIT_weight.wts", "4BIT_bias.bss");
 	NN1.accuracy(true);
 	NN1.testing(true);
 }
+
 
 
